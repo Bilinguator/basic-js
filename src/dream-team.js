6d3f1ebde471cc51d047c86303d51d3f75a0cdc7
Dmitrii Snitkin <snitkin.d@list.ru>
2020-09-28 17:05:13 +0300

    feat: implement dream-team function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  if (!Array.isArray(arr))
+    return false;
+  
+  let resultArr = '';
+
+  arr.forEach(function (item) {
+    if (typeof item !== 'string')
+      return false;
+    else
+      resultArr += item.trim()[0].toUpperCase();
+  });
+
+  return resultArr.split('').sort().join('');
 };
