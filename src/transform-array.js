642ca9b12f21d1b461c5d801cd0b8cf168dbaf7b
Dmitrii Snitkin <snitkin.d@list.ru>
2020-09-29 00:24:05 +0300

    doesn`t change initial array

--- src/transform-array.js
+++ src/transform-array.js
@@ -10,34 +10,39 @@ module.exports = function transform(arr) {
   }
 
   let arrResult = [];
-  let arrCopy = arr;
+  let arrCopy = [];
+
+  for(let i = 0; i < arr.length; i++) {
+    arrCopy[i] = arr[i];
+  }
+
+  
 
   for(let i = 0; i < arrCopy.length; i++) {
 
     if (arrCopy[i] === '--discard-prev') {
-      if(arrCopy[i - 1])
+      if(i !== 0)
         arrCopy[i - 1] = 'blabla';
       arrCopy[i] = 'blabla';
       continue;
 
     } else if (arrCopy[i] === '--discard-next') {
-      if (arrCopy[i + 1])
+      if (i !== arrCopy.length - 1)
         arrCopy[i + 1] = 'blabla';
       arrCopy[i] = 'blabla';
       continue;
 
     } else if (arrCopy[i] === '--double-next') {
-      if(arrCopy[i + 1])
+      if (i !== arrCopy.length - 1)
         arrCopy[i] = arrCopy[i + 1];
       continue;
 
     } else if (arrCopy[i] === '--double-prev') {
-      if(arrCopy[i - 1])
+      if(i !== 0)
         arrCopy[i] = arrCopy[i - 1];
       continue;
     } else {
       continue;
-
     }
   }
 
@@ -46,4 +51,31 @@ module.exports = function transform(arr) {
       arrResult.push(arrCopy[i]);
   }
   return arrResult;
+
+/*  let arrResult = [];
+  let index = 0;
+
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-prev') {
+      if (index !== 1)
+        arrResult.pop();
+      index = 0;
+    } else if (arr[i] === '--discard-next') {
+      i++;
+      index = 1;
+    } else if (arr[i] === '--double-next') {
+      if (i !== arr.length - 1)
+        arrResult.push(arr[i + 1]);
+      index = 0;
+    } else if if (arr[i] === '--double-prev') {
+      if (i !== 0)
+        arrResult.push(arr[i - 1]);
+      index = 0;
+    } else {
+      arrResult.push(arr[i]);
+      index = 0;
+    }
+  }
+
+  return arrResult;*/
 };
