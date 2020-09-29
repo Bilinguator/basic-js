3b744f79a70b863b625522a96b7eda5a8975a895
Dmitrii Snitkin <snitkin.d@list.ru>
2020-09-29 11:54:04 +0300

    fix: fix transform-array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -10,15 +10,42 @@ module.exports = function transform(arr) {
   }
 
   let arrResult = [];
-  let arrCopy = [];
 
-  for(let i = 0; i < arr.length; i++) {
-    arrCopy[i] = arr[i];
+  let index = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-prev') {
+      if (index !== 1)
+        arrResult.pop();
+      index = 0;
+    } else if (arr[i] === '--discard-next') {
+      i++;
+      index = 1;
+    } else if (arr[i] === '--double-next') {
+      if (i !== arr.length - 1)
+        arrResult.push(arr[i + 1]);
+      index = 0;
+    } else if (arr[i] === '--double-prev') {
+      if (i !== 0 && index !== 1)
+        arrResult.push(arr[i - 1]);
+      index = 0;
+    } else {
+      arrResult.push(arr[i]);
+      index = 0;
+    }
   }
 
-  
+  return arrResult;
 
-  for(let i = 0; i < arrCopy.length; i++) {
+  //Второй вариант решения. Плохой, так как в массиве не должно находиться строки 'blabla'
+
+  /* let arrCopy = [];
+
+  for(let i = 0; i < arr.length; i++) {
+    arrCopy.push(arr[i]);
+  }
+
+  for (let i = 0; i < arrCopy.length; i++) {
 
     if (arrCopy[i] === '--discard-prev') {
       if(i !== 0)
@@ -35,11 +62,15 @@ module.exports = function transform(arr) {
     } else if (arrCopy[i] === '--double-next') {
       if (i !== arrCopy.length - 1)
         arrCopy[i] = arrCopy[i + 1];
+      else
+        arrCopy[i] = 'blabla';
       continue;
 
     } else if (arrCopy[i] === '--double-prev') {
-      if(i !== 0)
+      if (i !== 0)
         arrCopy[i] = arrCopy[i - 1];
+      else
+        arrCopy[i] = 'blabla';
       continue;
     } else {
       continue;
@@ -50,32 +81,5 @@ module.exports = function transform(arr) {
     if(arrCopy[i] !== 'blabla')
       arrResult.push(arrCopy[i]);
   }
-  return arrResult;
-
-/*  let arrResult = [];
-  let index = 0;
-
-  for(let i = 0; i < arr.length; i++) {
-    if (arr[i] === '--discard-prev') {
-      if (index !== 1)
-        arrResult.pop();
-      index = 0;
-    } else if (arr[i] === '--discard-next') {
-      i++;
-      index = 1;
-    } else if (arr[i] === '--double-next') {
-      if (i !== arr.length - 1)
-        arrResult.push(arr[i + 1]);
-      index = 0;
-    } else if if (arr[i] === '--double-prev') {
-      if (i !== 0)
-        arrResult.push(arr[i - 1]);
-      index = 0;
-    } else {
-      arrResult.push(arr[i]);
-      index = 0;
-    }
-  }
-
-  return arrResult;*/
+  return arrResult; */
 };
